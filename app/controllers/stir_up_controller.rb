cb3b16a8e2955e0b88830e66cc86f3ad9964fe8d
pyama86 <pyama@pepabo.com>
2018-04-21 16:17:03 +0900

    dump params

--- app/controllers/stir_up_controller.rb
+++ app/controllers/stir_up_controller.rb
@@ -1,6 +1,7 @@
 class StirUpController < ApplicationController
   def create
     request = AlexaRubykit::build_request(params)
+    Rails.logger.debug params
     response = AlexaRubykit::Response.new
     response.add_speech("あれ？小山さん、まだそこの実装やってるんですか？")
     render json: response.build_response
