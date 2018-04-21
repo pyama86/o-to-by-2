968b45fb6021d38ea961ab64b41793c2fe9c472f
pyama86 <pyama@pepabo.com>
2018-04-22 00:36:44 +0900

    c

--- app/controllers/stir_up_controller.rb
+++ app/controllers/stir_up_controller.rb
@@ -3,7 +3,7 @@ class StirUpController < ApplicationController
     request = AlexaRubykit::build_request(params)
     #Rails.logger.debug params
     response = AlexaRubykit::Response.new
-    response.add_speech("あれ？小山さん、まだそこの実装やってるんですか？")
+    response.add_speech("あれ？#{request.slots[:city][:value]}、まだそこの実装やってるんですか？")
     render json: response.build_response
   end
 end
