31f8d9ff64bf7f5daa6e7fdb0fbce4620d31c128
pyama86 <pyama@pepabo.com>
2018-04-22 09:58:50 +0900

    add weather

--- Gemfile
+++ Gemfile
@@ -42,3 +42,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'alexa_rubykit'
+
+gem 'weather-report'
--- app/controllers/stir_up_controller.rb
+++ app/controllers/stir_up_controller.rb
@@ -3,7 +3,16 @@ class StirUpController < ApplicationController
     request = AlexaRubykit::build_request(params)
     #Rails.logger.debug params
     response = AlexaRubykit::Response.new
-    response.add_speech("あれ？#{request.slots[:city][:value]}、まだそこの実装やってるんですか？")
+
+    city = request.slots[:city][:value]
+    id = WeatherReport::Weather.request_cityid(city)
+    if id
+      weather = WeatherReport::Weather.new(id)
+      weather.tomorrow.date
+      response.add_speech("#{city}の明日の天気は#{weather.tomorrow.telop}です")
+    else
+      response.add_speech("#{city}の天気が調べられませんでした")
+    end
     render json: response.build_response
   end
 end
